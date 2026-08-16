216b1df1910de88df88986b3fc3804f54203a8a3
KingBacconnnn <baserushing57@gmail.com>
2026-08-16 22:23:44 +0800

    Update untitled.lua

--- untitled.lua
+++ untitled.lua
@@ -1789,7 +1789,7 @@ local function CreateScriptCard(data, renderParent)
 	table.insert(RegisteredScripts, scriptEntry)
 end
 
-local CATALOG_URL = "https://raw.githubusercontent.com/KingBacconnnn/VeloxScripts/refs/heads/main/catalogtest.json"
+local CATALOG_URL = "https://raw.githubusercontent.com/KingBacconnnn/VeloxScripts/refs/heads/main/catalog.json"
 local CATALOG_REFRESH_INTERVAL = 300
 local dbRefreshing = false
 local CatalogRefreshQueued = false
